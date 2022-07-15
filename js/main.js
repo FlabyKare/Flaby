5da1596e3cbc356116cd761e86157c8ed7dec71c
FlabyKare <beslansoblirov074@gmail.com>
2022-07-15 11:24:45 +0300

    menu_fixed

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
 let burger = $('.burger')
 let menu_wrapper = $('.menu_wrapper')
 let body = $('body')
-let burger_open = $('.burger_open')
+let body_cont = $('.body_container')
 
 burger.click(function(){
     menu_wrapper.toggleClass('menu_open')
     burger.toggleClass('transform_rotate')
     body.toggleClass('overflow_hidden')
+    body_cont.toggleClass('d-none')
 
 })
 
