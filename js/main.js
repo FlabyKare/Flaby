6b77ce671004f6bfe7b5277f81aa4f7d7e6af999
FlabyKare <beslansoblirov074@gmail.com>
2022-07-19 13:11:17 +0300

    added_function_close_menu

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@ let burger = $('.burger')
 let menu_wrapper = $('.menu_wrapper')
 let body = $('body')
 let body_cont = $('.body_container')
+let menu_link = $('.menu_link')
 
 burger.click(function(){
     menu_wrapper.toggleClass('menu_open')
     burger.toggleClass('transform_rotate')
     body.toggleClass('overflow_hidden')
     body_cont.toggleClass('d-none')
+})
+
+menu_link.click(function(){
+    menu_wrapper.toggleClass('menu_open')
+    burger.toggleClass('transform_rotate')
+    body.toggleClass('overflow_hidden')
+    body_cont.toggleClass('d-none')
 
 })
+
 
 });
 
